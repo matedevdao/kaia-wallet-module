5e6a1a2e9cfcd9d5c3836a8e85a44e1c8d525901
yjgaia <yjdev88@gmail.com>
2025-02-22 22:54:14 +0900

    Add QR code generation support to KlipConnector and update dependencies

--- src/wallet-connectors/KlipConnector.ts
+++ src/wallet-connectors/KlipConnector.ts
@@ -1,4 +1,8 @@
 import WalletForKaiaConnector from "./WalletForKaiaConnector.js";
+import QrCode from "qrcode";
+
+// @ts-ignore
+import { getResult, prepare, request } from "klip-sdk";
 
 class KlipConnector implements WalletForKaiaConnector {}
 
