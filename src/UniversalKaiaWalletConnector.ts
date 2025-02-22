e144a3ba185df70ae9e11105e38640ac78ae62e5
yjgaia <yjdev88@gmail.com>
2025-02-22 23:24:41 +0900

    Add support for multiple wallet connectors in UniversalKaiaWalletConnector

--- src/UniversalKaiaWalletConnector.ts
+++ src/UniversalKaiaWalletConnector.ts
@@ -1,7 +1,16 @@
+import KaiaWalletConnector from "./wallet-connectors/KaiaWalletConnector.js";
+import KlipConnector from "./wallet-connectors/KlipConnector.js";
+import MetaMaskConnector from "./wallet-connectors/MetaMaskConnector.js";
+import WalletConnectConnector from "./wallet-connectors/WalletConnectConnector.js";
 import WalletForKaiaConnector from "./wallet-connectors/WalletForKaiaConnector.js";
 
 class UniversalKaiaWalletConnector {
-  public connectors: WalletForKaiaConnector[] = [];
+  public connectors: WalletForKaiaConnector[] = [
+    KaiaWalletConnector,
+    KlipConnector,
+    MetaMaskConnector,
+    WalletConnectConnector,
+  ];
 
   public init(walletId?: string) {
   }
