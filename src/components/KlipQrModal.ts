3d6a9dfb507004c90693b56513e3225650f928f3
yjgaia <yjdev88@gmail.com>
2025-02-23 13:50:28 +0900

    Implement KlipQrModal with QR code display and localized messages

--- src/components/KlipQrModal.ts
+++ src/components/KlipQrModal.ts
@@ -1,7 +1,34 @@
-import { StructuredModal } from "@common-module/app-components";
+import { el, msg } from "@common-module/app";
+import {
+  AppCompConfig,
+  Button,
+  ButtonType,
+  StructuredModal,
+} from "@common-module/app-components";
 
 export default class KlipQrModal extends StructuredModal {
   constructor(title: string, qr: string) {
     super(".klip-qr-modal");
+
+    this.appendToHeader(
+      el("h1", title),
+      new Button(".close", {
+        type: ButtonType.Icon,
+        icon: new AppCompConfig.CloseIcon(),
+        onClick: () => this.remove(),
+      }),
+    );
+
+    this.appendToMain(
+      el(".qr", el("img", { src: qr })),
+      el("p", msg("klip_qr_modal.guide")),
+    );
+
+    this.appendToFooter(
+      new Button({
+        title: msg("klip_qr_modal.button.cancel"),
+        onClick: () => this.remove(),
+      }),
+    );
   }
 }
