d42cb67f42b561e3ccfb33e4284b54207f2536a9
yjgaia <yjdev88@gmail.com>
2025-03-07 14:29:41 +0900

    Refactor KaiaWalletConnector to dynamically determine provider based on device type

--- src/wallet-connectors/KaiaWalletConnector.ts
+++ src/wallet-connectors/KaiaWalletConnector.ts
@@ -1,3 +1,4 @@
+import { BrowserInfo } from "@common-module/app";
 import { injected } from "@wagmi/connectors";
 import KaiaWalletIcon from "../components/wallet-icons/KaiaWalletIcon.js";
 import WagmiWalletConnector from "./WagmiWalletConnector.js";
@@ -8,17 +9,19 @@ class KaiaWalletConnector extends WagmiWalletConnector {
   public walletIcon = new KaiaWalletIcon();
 
   public init() {
+    let provider = (window as any).klaytn;
+    if (!provider) {
+      if (BrowserInfo.isMobileDevice()) {
+        provider = (window as any).ethereum;
+      } else {
+        provider = (window as any).ethereum?.isKaikas
+          ? (window as any).ethereum
+          : undefined;
+      }
+    }
     this.wagmiConnector = injected({
       target() {
-        return {
-          id: "kaia-wallet",
-          name: "Kaia Wallet",
-          provider: (window as any).klaytn ?? (
-            (window as any).ethereum?.isKaikas
-              ? (window as any).ethereum
-              : undefined
-          ),
-        };
+        return { id: "kaia-wallet", name: "Kaia Wallet", provider };
       },
     });
   }
